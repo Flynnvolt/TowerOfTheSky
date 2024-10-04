dcb64ddf89ccc39fee92ff0ccce04d9d7221174e
Flynnvolt <105185949+Flynnvolt@users.noreply.github.com>
2024-10-04 07:59:10 -0700

    trying to get release to work

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+
 if not exist build (
 	mkdir build
 )
@@ -6,6 +7,5 @@ if not exist build (
 pushd build
 
 "clang" -g -fuse-ld=lld -fdiagnostics-absolute-paths -o "cgame.exe" "..\build.c" -O0 -std=c11 -D_CRT_SECURE_NO_WARNINGS -Wextra -Wno-incompatible-library-redeclaration -Wno-sign-compare -Wno-unused-parameter -Wno-builtin-requires-header -lkernel32 -lgdi32 -luser32 -lruntimeobject -lwinmm -ld3d11 -ldxguid -ld3dcompiler -lshlwapi -lole32 -lshcore -lavrt -lksuser -ldbghelp -femit-all-decls -Wno-microsoft-enum-forward-reference
-
 
 popd
--- build_release.bat
+++ build_release.bat
@@ -8,4 +8,6 @@ pushd build_release
 
 clang -o cgame.exe ../build.c -Ofast -DNDEBUG -std=c11 -D_CRT_SECURE_NO_WARNINGS -Wextra -Wno-incompatible-library-redeclaration -Wno-sign-compare -Wno-unused-parameter -Wno-builtin-requires-header -Wno-deprecated-declarations -lkernel32 -lgdi32 -luser32 -lruntimeobject -lwinmm -ld3d11 -ldxguid -ld3dcompiler -lshlwapi -lole32 -lshcore -lavrt -lksuser -finline-functions -finline-hint-functions -ffast-math -fno-math-errno -funsafe-math-optimizations -freciprocal-math -ffinite-math-only -fassociative-math -fno-signed-zeros -fno-trapping-math -ftree-vectorize -fomit-frame-pointer -funroll-loops -fno-rtti -fno-exceptions -Wno-microsoft-enum-forward-reference
 
+xcopy "..\TowerOfTheSky\Resources" "Resources" /E /I /Y
+
 popd
